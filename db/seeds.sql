4d4aabd9f28b7dc266ed364e3c04636d8b53d666
Adriana Garcia-Trevino <adrianagarcia-trevino@Adrianas-MacBook-Pro.local>
2017-11-05 20:23:23 -0600

    second change

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
 insert into burgers (burger_name) values ("cheese burger");
 insert into burgers (burger_name) values ("veggie burger");
+insert into burgers (burger_name) values ("mexican burger");
